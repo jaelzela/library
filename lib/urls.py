337a8f6e04587d81127162c58d484ab4261e82a0
jzelar <jzelar@gmail.com>
2017-03-15 12:39:28 -0500

    Search, delete, filters

--- lib/urls.py
+++ lib/urls.py
@@ -6,7 +6,10 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'login', views.login_user, name='login'),
     url(r'logout', views.logout_user, name='logout'),
+    url(r'signup', views.signup_user, name='signup'),
     url(r'book/([a-z0-9]+)/$', views.book, name='book'),
     url(r'book/([a-z0-9]+)/edit$', views.book_edit, name='book_edit'),
+    url(r'book/([a-z0-9]+)/delete$', views.book_delete, name='book_delete'),
     url(r'authors', views.authors, name='authors'),
+    url(r'search', views.search, name='search'),
 ]
--- lib/views.py
+++ lib/views.py
@@ -1,18 +1,38 @@
-from django.core.files.storage import FileSystemStorage
-from django.core.paginator import Paginator, InvalidPage, EmptyPage
+# -*- coding: utf-8 -*-
+__author__ = "Jael Zela"
+
+from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.core.files.storage import FileSystemStorage
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.db import IntegrityError
+from django.db.models import Q
 from django.http import HttpResponseRedirect, JsonResponse, Http404
-from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render
-from django.conf import settings
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_protect
 
 from lib.models import *
+import operator
+import os
 
 
 @login_required(login_url='/lib/login')
 def index(request):
     books = Book.objects.all()
+
+    authors = []
+    years = []
+    for b in books:
+        authors += [a.name for a in b.authors.all()]
+        years.append(b.publication_year)
+    authors = list(set(authors))
+    authors.sort()
+    years = list(set(years))
+    years.sort()
+
     paginator = Paginator(books, 10)
 
     try:
@@ -25,7 +45,7 @@ def index(request):
     except (InvalidPage, EmptyPage):
         books = paginator.page(paginator.num_pages)
 
-    return render(request, 'lib/index.html', dict(books=books))
+    return render(request, 'lib/index.html', dict(books=books, authors=authors, years=years))
 
 
 @csrf_protect
@@ -33,7 +53,7 @@ def login_user(request):
     if request.method == 'GET':
         return render(request, 'lib/login.html', dict())
 
-    if request.method == 'POST':
+    elif request.method == 'POST':
         email = request.POST.get('email')
         password = request.POST.get('password')
 
@@ -51,14 +71,34 @@ def login_user(request):
 
         return render(request, 'lib/login.html', dict(error=login_error))
 
+    else:
+        raise Http404
+
 
 def logout_user(request):
-    if request.method == 'GET':
-        next = request.GET.get('next', '')
     logout(request)
-    if next:
-        return HttpResponseRedirect(next)
     return HttpResponseRedirect('/')
+
+
+def signup_user(request):
+    if request.method == 'GET':
+        return render(request, "lib/signup.html", dict())
+    elif request.method == 'POST':
+        first_name = request.POST.get('first_name', '')
+        email = request.POST.get('email', '').lower()
+        password = request.POST.get('password', '')
+
+        try:
+            user = User.objects.create_user(username=email, email=email, password=password, first_name=first_name, last_login=timezone.now())
+            user.is_active = True
+            user.save()
+        except IntegrityError:
+            error = "Email address already registered."
+            return render(request, "lib/signup.html", dict(signup_error=error))
+
+        return render(request, "lib/confirmation.html", dict())
+    else:
+        return HttpResponseRedirect('/')
 
 
 @login_required(login_url='/lib/login')
@@ -131,10 +171,75 @@ def book_edit(request, book_id):
         raise Http404
 
 
+@login_required(login_url='/lib/login')
+def book_delete(request, book_id):
+    if request.method == 'GET':
+        try:
+            book = Book.objects.get(id=int(book_id))
+            url_image = book.cover_image
+            book.delete()
+
+            os.remove(os.path.join(settings.LIB_MEDIA_DIR, url_image.split('/')[-1]))
+
+            return HttpResponseRedirect('/lib')
+        except ValueError:
+            raise Http404
+    else:
+        raise Http404
+
+
 @csrf_protect
 def authors(request):
     authors = [a.as_json() for a in Author.objects.all()]
     return JsonResponse({'authors': authors})
 
-#INSERT INTO `auth_user` VALUES
-#(1,'pbkdf2_sha256$24000$EYtAEeWO79qO$sEAFlWwFaZgxxkjNHZxhB8BwKHGZx22xySbWUxWvQRk=','2017-02-27 02:27:43',0,'jzelar@gmail.com','Jael','','jzelar@gmail.com',0,1,'2017-01-28 23:45:19');
+
+@login_required(login_url='/lib/login')
+def search(request):
+    if request.method == 'GET':
+        query = request.GET.get('q', '')
+        f_author = request.GET.get('f_author', '')
+        f_year = request.GET.get('f_year', '')
+
+        filters = []
+        filter_labels = []
+        if f_year:
+            filters.append(Q(publication_year=f_year))
+            filter_labels.append("Publication Year: "+f_year)
+        if f_author:
+            filters.append(Q(authors__name=f_author))
+            filter_labels.append('Author: "'+f_author+'"')
+
+        if len(filters) > 0:
+            books = Book.objects.filter(Q(title__icontains=query) | Q(authors__name__icontains=query), reduce(operator.and_, filters))
+        else:
+            books = Book.objects.filter(Q(title__icontains=query) | Q(authors__name__icontains=query))
+
+        authors = []
+        years = []
+        for b in books:
+            authors += [a.name for a in b.authors.all()]
+            years.append(b.publication_year)
+        authors = list(set(authors))
+        authors.sort()
+        years = list(set(years))
+        years.sort()
+
+        paginator = Paginator(books, 10)
+
+        try:
+            page_number = int(request.GET.get('page', '1'))
+        except ValueError:
+            page_number = 1
+
+        try:
+            books = paginator.page(page_number)
+        except (InvalidPage, EmptyPage):
+            books = paginator.page(paginator.num_pages)
+
+        q_label = 'title: "'+query+'" OR author: "'+query+'"' if query else ''
+        f_label = ' AND '.join(filter_labels)
+        print q_label, f_label
+        return render(request, 'lib/index.html', dict(books=books, authors=authors, years=years, q_label=q_label, f_label=f_label))
+    else:
+        raise Http404
